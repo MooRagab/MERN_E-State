0af6a3d38591eed7595a9adfdd36d6a6ee466777
Mohamed Ragab <rmidoragab222@gmail.com>
2024-06-24 12:59:22 +0300

    Connnect to mongoDB and some edits

--- API/index.js
+++ API/index.js
@@ -1,6 +1,31 @@
+import path from "path";
+import { fileURLToPath } from "url";
+import dotenv from "dotenv";
 import express from "express";
-const app = express();
-const port = 3000;
+import connectDB from "./DB/connection.js";
 
-app.get("/", (req, res) => res.send("Hello World!"));
+//Set Directory dirname
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+dotenv.config({ path: path.join(__dirname, "./config/.env") });
+
+const app = express();
+const port = process.env.PORT;
+const baseUrl = process.env.BASEURL;
+
+//convert Buffer Data
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get(`${baseUrl}/`, (req, res) => {
+  res.send("success");
+});
+
+//In-Valid URL
+app.get("*", (req, res) => {
+  res.status(404).send("Error 404 Not Found");
+});
+
+//Connections
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+connectDB();
+ 
