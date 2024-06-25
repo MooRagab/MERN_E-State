1f8d5d34989c473033d92515c4dbb2926fbb89bb
Mohamed Ragab <rmidoragab222@gmail.com>
2024-06-25 08:04:21 +0300

    add sendEnail function

--- API/index.js
+++ API/index.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import express from "express";
 import connectDB from "./DB/connection.js";
 import userRouter from "./src/routes/user.route.js";
+import authRouter from "./src/routes/auth.route.js";
 
 //Set Directory dirname
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
@@ -13,7 +14,12 @@ const app = express();
 const port = process.env.PORT;
 const baseUrl = process.env.BASEURL;
 
+//convert Buffer Data
+app.use(express.json());
+
 app.use(`${baseUrl}/user`, userRouter);
+app.use(`${baseUrl}/auth`, authRouter);
+
 //In-Valid URL
 app.get("*", (req, res) => {
   res.status(404).send("Error 404 Not Found");
---
+++ API/src/controllers/auth.controller.js
@@ -0,0 +1,6 @@
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+export const signup = (req, res) => {
+  console.log(req.body);
+};
